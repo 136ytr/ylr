eb6f2b4c4d649f955bce4372c673cef1aac1add4
136ytr <44334104+136ytr@users.noreply.github.com>
2020-12-08 20:52:08 -0600

    Update run.py

--- run.py
+++ run.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!!'
- '''
+
 
 @app.route('/')
 def ran():
     return render_template("ran.html")
+'''
 
+@app.route('/')
+def pdf():
+    return render_template("pdf.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
